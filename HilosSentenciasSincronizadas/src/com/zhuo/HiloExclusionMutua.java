21b75fcf2c5a4bd6d03fb0fa043856110943ae92
zhuoIC <nicolimonar@gmail.com>
2017-11-02 13:11:25 +0100

    HiloSaludos: ejemplo con wait() y notify()

--- HilosSentenciasSincronizadas/src/com/zhuo/HiloExclusionMutua.java
+++ HilosSentenciasSincronizadas/src/com/zhuo/HiloExclusionMutua.java
@@ -4,12 +4,23 @@ class DatoCompartido{
 	public static int c1 = 0;
 	public static int c2 = 0;
 	
+	private static final Object mutex1 = new Object();
+	private static final Object mutex2 = new Object();
+
 	public static void incrementarC1() {
-		c1++;
+		// SC1
+		synchronized (mutex1) {
+			c1++;
+		}
+		// Fin SC1
 	}
 	
 	public static void incrementarC2() {
-		c2++;
+		// SC2
+		synchronized (mutex2) {
+			c2++;
+		}
+		// Fin SC2
 	}
 
 }
@@ -30,7 +41,7 @@ public class HiloExclusionMutua {
 	public static void main(String[] args) throws InterruptedException {
 		int n = Integer.parseInt(args[0]);
 		HilosMutex hilos[];
-		System.out.println("Creando N hilos");
+		System.out.println("Creando "+ n +" hilos");
 		hilos = new HilosMutex[n];
 		for (int i = 0; i < hilos.length; i++) {
 			hilos[i] = new HilosMutex();
