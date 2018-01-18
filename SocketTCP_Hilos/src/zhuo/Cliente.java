b640f944ec68f966afe3b97708086ae38c4237e8
zhuoIC <nicolimonar@gmail.com>
2018-01-18 12:34:26 +0100

    Ejercicio de sockets terminado.
    El servidor espera un mensaje del cliente.

--- SocketTCP_Hilos/src/zhuo/Cliente.java
+++ SocketTCP_Hilos/src/zhuo/Cliente.java
@@ -3,13 +3,16 @@ package zhuo;
 import java.io.IOException;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
 import java.net.Socket;
 import java.net.UnknownHostException;
 import java.util.Scanner;
 
 public class Cliente {
 	
-	static final String HOST = "192.168.2.40";
+	static final String HOST = "192.168.3.57";
 	static final int PUERTO = 5000;
 	Scanner entrada;
 	
@@ -21,19 +24,24 @@ public class Cliente {
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		while (true) {
-			new Cliente();			
-		}
-		
+			new Cliente();				
 	}
 	
 	public Cliente() {
 		try {
 			Socket skCli = new Socket(HOST, PUERTO);
-			InputStreamReader is = new InputStreamReader(skCli.getInputStream(), "utf-8");
-			BufferedReader br = new BufferedReader(is);
-			
+			//InputStreamReader is = new InputStreamReader(skCli.getInputStream(), "utf-8");
+			BufferedReader br = new BufferedReader(new InputStreamReader(skCli.getInputStream(), "utf-8"));
+			BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in, "utf-8"));
+			PrintWriter pw = new PrintWriter(new OutputStreamWriter(skCli.getOutputStream(), "utf-8"), true);
+
 			System.out.println(br.readLine()); // Recibimos el saludo del servidor
+			
+			// enviar mensaje pedidio por consola al servidor
+			System.out.print("Mensaje para enviar: ");
+			String mensaje = teclado.readLine();
+			System.out.println("Enviando al servidor el mensaje: " + mensaje);
+			pw.println(mensaje);
 			
 			skCli.close();
 			
--- SocketTCP_Hilos/src/zhuo/SevidorHilo.java
+++ SocketTCP_Hilos/src/zhuo/SevidorHilo.java
@@ -5,6 +5,7 @@ import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
 
@@ -22,18 +23,21 @@ public class SevidorHilo extends Thread{
 	
 	@Override
 	public void run() {
-		BufferedOutputStream bo;
-		BufferedInputStream is;
+		//BufferedOutputStream bo;
+		//BufferedInputStream is;
 		BufferedReader br;
 		PrintWriter pw = null;
 		
 		try {
-			bo = new BufferedOutputStream(socket.getOutputStream());
-			pw = new PrintWriter(bo, true); // En true limpia el buffer
+			//bo = new BufferedOutputStream(socket.getOutputStream());
+			pw = new PrintWriter(new OutputStreamWriter(socket.getOutputStream(), "utf-8"), true); // En true limpia el buffer
 			pw.println(id + ":" + this.mensaje);
 			
 			// Y ahora espera una respuesta en forma de string desde el cliente
-			//br = new BufferedReader(new InputStreamReader(System.in))
+			br = new BufferedReader(new InputStreamReader(socket.getInputStream(), "utf-8"));
+			System.out.print("Mensaje recibido desde el cliente: ");
+			System.out.println(br.readLine());
+			
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
